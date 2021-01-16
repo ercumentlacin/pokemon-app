ad325fbfd333d1453f2c72dc827b383801427b94
ercüment <41874858+ercumentlacin@users.noreply.github.com>
2021-01-16 15:09:58 +0300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 
 import ThemeContext from "../context/ThemeContext";
-
+import CharacterDetail from "../components/CharacterDetail";
 const About = () => {
   // theme changes area
   const { theme } = useContext(ThemeContext);
@@ -12,9 +12,11 @@ const About = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios("https://pokeapi.co/api/v2/pokemon?limit=151");
+      const { data } = await axios(
+        "https://pokeapi.co/api/v2/pokemon?limit=151"
+      );
 
-      setData(result.data.results);
+      setData(data.results);
     };
     fetchData();
   }, []);
@@ -24,25 +26,8 @@ const About = () => {
       <h1 className="d-flex justify-content-center display-2">PokeDex</h1>
       <div>
         <div className="row">
-          {data.map((item, index) => (
-            <div key={index} className="col-sm-6 col-md-4 col-lg-3 my-3 ">
-              <div
-                className={`card p-3 text-capitalize mx-auto bg-${theme}`}
-                style={{ maxWidth: 18 + "rem" }}
-              >
-                <img
-                  src={`https://pokeres.bastionbot.org/images/pokemon/${index +
-                    1}.png`}
-                  alt={item.name}
-                  className="card-img-top"
-                />
-                <div className="card-body">
-                  <h4>{item.name}</h4>
-                  <p className="card-text">{item.name}</p>
-                  <a href={item.url}>link</a>
-                </div>
-              </div>
-            </div>
+          {data?.map((item, index) => (
+            <CharacterDetail key={index} index={index} item={item} />
           ))}
         </div>
       </div>
